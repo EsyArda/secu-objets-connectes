eaf44c4d50b5a8a76746592d82fcb20875d147ed
Lilian Hiault <lilian.hiault@laposte.net>
2021-09-29 15:37:11 +0200

    Regroupé fichiers python et ajout fonctions decode

--- S3 - Chaine de sécurité/lib_hex/decode_hex.py
+++ S3 - Chaine de sécurité/lib_hex/decode_hex.py
@@ -34,28 +34,43 @@ def decode_file(file_name, out="out.txt", length=0):
                 byte_count = record[1:3]
                 if is_type_data(record):
                     data = record[9:9 + 2 * int(byte_count, base=16)]
-                    str_ascii = hex_to_ascii(data, length_min=length)
+                    str_ascii = hex_to_ascii(data, n=length)
                     f2.write(str_ascii + "\n")
 
 
-def hex_to_ascii(data, length_min=0):
+def hex_to_ascii(data, n=1):
+    """Returns ASCII string of more than n cnosecutive printable characters from data"""
     s = ""
     temp_s = ""
     for i in range(0, len(data), 2):
         if data[i:i+2].isprintable():
             temp_s += chr(int(data[i:i+2], 16))
         else:
-            if len(temp_s) >= length_min:
+            if len(temp_s) >= n:
                 s += temp_s
-                temp_s = ""
             temp_s = ""
-    if len(temp_s) >= length_min:
+    if len(temp_s) >= n:
         s += temp_s
     return s
 
+def byte_count(record):
+    return record[1:3]
+
+def adress(record):
+    return record[3:7]
+
+def record_type(record):
+    return record[7:9]
+
+def data(record):
+    return record[9:-2]
+
+def checksum(record):
+    return record[-2:]
+
 
 def main():
-    decode_file("./S3 - Chaine de sécurité/firmware_tp3_v2.33.hex", length=3)
+    decode_file("./S3 - Chaine de sécurité/firmware_tp3_v2.33.hex", out="./S3 - Chaine de sécurité/out5.txt", length=5)
 
 
 if __name__ == "__main__":
