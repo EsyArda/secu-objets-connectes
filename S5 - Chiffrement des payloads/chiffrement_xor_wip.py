8c85266c0aa636f01966ec6a41707206813f2b31
Lilian Hiault <lilian.hiault@laposte.net>
2021-10-07 16:13:46 +0200

    chiffrement Xor Python

--- S5 - Chiffrement des payloads/chiffrement_xor_wip.py
+++ S5 - Chiffrement des payloads/chiffrement_xor_wip.py
@@ -1,7 +1,14 @@
+def charXor(a, b):
+    """Returns character a xor b"""
+    return chr(ord(a) ^ ord(b))
 
 def Xor(inp,key):
-    #TODO
-    return []
+    s = ""
+    i = 0
+    for c in inp:
+        s += charXor(c, key[i])
+        i = (i + 1) % len(key)
+    return s
 
 def toCdecl(ch,prefix="char tab"):
     outp=list(ch)
@@ -13,3 +20,9 @@ if __name__ == '__main__':
     print (toCdecl(Xor("TRAVAUX PRATIQUES ISIMA","F5")))
     print (toCdecl(Xor("TRAVAUX PRATIQUES ISIMA","ZZ2_F5")))
     print (toCdecl(Xor("ZZ2_F5","F5")))
+    
+    print(Xor(Xor("TRAVAUX PRATIQUES ISIMA","F"), "F"))
+    print(Xor(Xor("TRAVAUX PRATIQUES ISIMA","5"), "5"))
+    print(Xor(Xor("TRAVAUX PRATIQUES ISIMA","F5"), "F5"))
+    print(Xor(Xor("TRAVAUX PRATIQUES ISIMA","ZZ2_F5"), "ZZ2_F5"))
+    print(Xor(Xor("ZZ2_F5","F5"), "F5"))
