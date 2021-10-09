138829d5d8a7495edc72bd530d4f7093b5fe4f78
Raikirin <91314577+RodriguesDylan@users.noreply.github.com>
2021-10-09 12:15:38 +0200

    Ajout du travail seance 5

--- S5 - Chiffrement des payloads/chiffrement_xor_wip.py
+++ S5 - Chiffrement des payloads/chiffrement_xor_wip.py
@@ -15,14 +15,15 @@ def toCdecl(ch,prefix="char tab"):
     return prefix+"[{}]".format(len(outp)+1)+r"""="\x"""+(r"\x".join(["{:02x}".format(ord(_)) for _ in outp]))+r"""";"""
 
 if __name__ == '__main__':
-    print (toCdecl(Xor("TRAVAUX PRATIQUES ISIMA","F")))
-    print (toCdecl(Xor("TRAVAUX PRATIQUES ISIMA","5")))
-    print (toCdecl(Xor("TRAVAUX PRATIQUES ISIMA","F5")))
-    print (toCdecl(Xor("TRAVAUX PRATIQUES ISIMA","ZZ2_F5")))
-    print (toCdecl(Xor("ZZ2_F5","F5")))
+    # print (toCdecl(Xor("TRAVAUX PRATIQUES ISIMA","F")))
+    # print (toCdecl(Xor("TRAVAUX PRATIQUES ISIMA","5")))
+    # print (toCdecl(Xor("TRAVAUX PRATIQUES ISIMA","F5")))
+    # print (toCdecl(Xor("TRAVAUX PRATIQUES ISIMA","ZZ2_F5")))
+    # print (toCdecl(Xor("ZZ2_F5","F5")))
     
-    print(Xor(Xor("TRAVAUX PRATIQUES ISIMA","F"), "F"))
-    print(Xor(Xor("TRAVAUX PRATIQUES ISIMA","5"), "5"))
-    print(Xor(Xor("TRAVAUX PRATIQUES ISIMA","F5"), "F5"))
-    print(Xor(Xor("TRAVAUX PRATIQUES ISIMA","ZZ2_F5"), "ZZ2_F5"))
-    print(Xor(Xor("ZZ2_F5","F5"), "F5"))
+    # print(Xor(Xor("TRAVAUX PRATIQUES ISIMA","F"), "F"))
+    # print(Xor(Xor("TRAVAUX PRATIQUES ISIMA","5"), "5"))
+    # print(Xor(Xor("TRAVAUX PRATIQUES ISIMA","F5"), "F5"))
+    # print(Xor(Xor("TRAVAUX PRATIQUES ISIMA","ZZ2_F5"), "ZZ2_F5"))
+    # print(Xor(Xor("ZZ2_F5","F5"), "F5")
+    print(toCdecl(Xor("AAAAA", "LOTRO")))
